1b138f2b1e774b8dae0b988f7ffe9aeeaf62c78e
Dhinesh <53396141+dhinesh-venkat@users.noreply.github.com>
2020-06-30 18:18:13 +0530

    Delete Rotate_array.py

--- Rotate_array.py
+++
@@ -1,9 +0,0 @@
-def rotate(nums,k):
-        n = len(nums)
-        k %= n
-        nums[:] = nums[n-k:] + nums[:n-k]
-        return nums
-
-nums = [1,2,3,4,5,6,7]
-k = 15000
-print(rotate(nums,k))
